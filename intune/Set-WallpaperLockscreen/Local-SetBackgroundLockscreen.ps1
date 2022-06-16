ddc436aa0aed5dad484158a2d7a23bc0d0345cfd
gnon17 <105109259+gnon17@users.noreply.github.com>
2022-06-15 21:11:48 -0500

    Add files via upload

---
+++ intune/Set-WallpaperLockscreen/Local-SetBackgroundLockscreen.ps1
@@ -0,0 +1,27 @@
+﻿#CreateRegKey
+New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP -Force
+
+#Variable Creation
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+$ImageDestinationFolder = "c:\MDM"
+$BackgroundFile = "background.jpg"
+$LockscreenFile = "lockscreen.jpg"
+$BackgroundImage = "$ImageDestinationFolder\$BackgroundFile"
+$LockScreenImage = "$ImageDestinationFolder\$LockscreenFile"
+
+#Create image directory
+md $ImageDestinationFolder -erroraction silentlycontinue
+
+#Move Files to local directory
+Copy-Item -Path .\$BackgroundFile -Destination $ImageDestinationFolder
+Copy-Item -Path .\$LockscreenFile -Destination $ImageDestinationFolder
+
+#Lockscreen Registry Keys
+New-ItemProperty -Path $RegPath -Name LockScreenImagePath -Value $LockScreenImage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name LockScreenImageUrl -Value $LockScreenImage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name LockScreenImageStatus -Value 1 -PropertyType DWORD -Force | Out-Null
+
+#Background Wallpaper Registry Keys
+New-ItemProperty -Path $RegPath -Name DesktopImagePath -Value $Backgroundimage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name DesktopImageUrl -Value $Backgroundimage -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $RegPath -Name DesktopImageStatus -Value 1 -PropertyType DWORD -Force | Out-Null
