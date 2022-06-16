38f087a1a89bf926cf38c4f12fe2108e9d8c67c0
gnon17 <105109259+gnon17@users.noreply.github.com>
2022-06-15 20:58:13 -0500

    Add files via upload

---
+++ teams/TeamsQoS-Uninstall.ps1
@@ -0,0 +1,3 @@
+﻿Remove-NetQosPolicy -Name "Teams Audio" -Confirm:$False
+Remove-NetQosPolicy -Name "Teams Video" -Confirm:$False
+Remove-NetQosPolicy -Name "Teams Sharing" -Confirm:$False
---
+++ teams/TeamsQoS.ps1
@@ -0,0 +1,3 @@
+new-NetQosPolicy -Name "Teams Audio" -AppPathNameMatchCondition "Teams.exe" -IPProtocolMatchCondition Both -IPSrcPortStartMatchCondition 50000 -IPSrcPortEndMatchCondition 50019 -DSCPAction 46 -NetworkProfile All
+new-NetQosPolicy -Name "Teams Video" -AppPathNameMatchCondition "Teams.exe" -IPProtocolMatchCondition Both -IPSrcPortStartMatchCondition 50020 -IPSrcPortEndMatchCondition 50039 -DSCPAction 34 -NetworkProfile All
+new-NetQosPolicy -Name "Teams Sharing" -AppPathNameMatchCondition "Teams.exe" -IPProtocolMatchCondition Both -IPSrcPortStartMatchCondition 50040 -IPSrcPortEndMatchCondition 50059 -DSCPAction 18 -NetworkProfile All
