9ed8cbaa92e0fcf39c8ca4b79398024ccde4577f
gnon17 <105109259+gnon17@users.noreply.github.com>
2022-06-23 11:21:11 -0500

    Add files via upload

---
+++ Hyper-V/New-VM.ps1
@@ -0,0 +1,14 @@
+$VMName = Read-Host 'Name your VM'
+MKDIR -Path C:\Hyper-V\"$VMName"
+Copy-Item -Path "C:\Hyper-V\W10_image\image.vhdx" -Destination "C:\Hyper-V\$VMname\$vmname.vhdx" | Out-Null
+New-VM -Name $VMName -Path C:\Hyper-V\$VMName -MemoryStartupBytes 4GB -VHDPath "C:\Hyper-V\$VMname\$vmname.vhdx" -Generation 2 -BootDevice VHD
+Set-VM -Name $VMName -ProcessorCount 2
+Set-VMMemory $VMName -DynamicMemoryEnabled $false
+Set-VM -Name $VMName -CheckpointType Disabled
+Connect-VMNetworkAdapter -VMName $VMName -SwitchName External
+
+#TPM
+$HGOwner = Get-HgsGuardian UntrustedGuardian
+$KeyProtector = New-HgsKeyProtector -Owner $HGOwner -AllowUntrustedRoot
+Set-VMKeyProtector -VMName $VMName -KeyProtector $KeyProtector.RawData
+Enable-VMTPM $VMName
