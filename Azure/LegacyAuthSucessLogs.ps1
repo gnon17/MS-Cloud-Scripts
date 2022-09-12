4bc536e31a78233aa5cf02e9f15c37cf6cb9d030
gnon17 <105109259+gnon17@users.noreply.github.com>
2022-09-12 14:40:12 -0500

    Add files via upload

---
+++ Azure/LegacyAuthSucessLogs.ps1
@@ -0,0 +1,20 @@
+﻿##Variables
+$BasicAuths = @('Autodiscover', 'Exchange ActiveSync', 'Exchange Online Powershell', 'Exchange Web Services', 'IMAP', 'MAPI Over HTTP', 'Offline Address Book', 'Other clients', 'Outlook Anywhere (RPC over HTTP)', 'POP', 'Reporting Web Services', 'SMTP', 'Universal Outlook')
+
+#Checking for AzureADPreview Module
+Write-Host -f Yellow "Checking if AzureADPreview Module is installed"
+if (Get-Module -ListAvailable -Name AzureADPreview) {
+    Write-Host "Module exists, continuing"
+} 
+else {
+    Write-Host "Module does not exist - Installing AzureADPreview Module"
+    Install-Module -Name AzureADPreview -Force
+}
+
+#Connect to AzureAD and pull legacy Auth Sign-ins
+Write-Host -f Yellow "Connecting to AzureAD...."
+Connect-AzureAD
+Get-AzureADAuditSignInLogs | where-object{$BasicAuths -eq $_.appDisplayName} | Format-Table -Property CreatedDateTime, UserPrincipalName, AppDisplayName, IPAddress, ClientAppUsed
+Write-Host -F Yellow "Script Complete - Press enter to disconnect from AzureAD"
+Pause
+Disconnect-AzureAD
