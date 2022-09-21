9d44054e504aba59f445641eb449d35db8830a89
gnon17 <105109259+gnon17@users.noreply.github.com>
2022-09-21 08:57:52 -0500

    Update LegacyAuthSucessLogs.ps1

--- Azure/LegacyAuthSucessLogs.ps1
+++ Azure/LegacyAuthSucessLogs.ps1
@@ -1,5 +1,5 @@
-﻿##Variables
-$BasicAuths = @('Autodiscover', 'Exchange ActiveSync', 'Exchange Online Powershell', 'Exchange Web Services', 'IMAP', 'MAPI Over HTTP', 'Offline Address Book', 'Other clients', 'Outlook Anywhere (RPC over HTTP)', 'POP', 'Reporting Web Services', 'SMTP', 'Universal Outlook')
+##Variables
+$BasicAuths = @('Autodiscover', 'Exchange ActiveSync', 'Exchange Online Powershell', 'Exchange Web Services', 'IMAP', 'MAPI Over HTTP', 'Offline Address Book', 'Other clients', 'Outlook Anywhere (RPC over HTTP)', 'POP', 'Reporting Web Services', 'SMTP', 'Authenticated SMTP', 'Universal Outlook')
 
 #Checking for AzureADPreview Module
 Write-Host -f Yellow "Checking if AzureADPreview Module is installed"
@@ -8,13 +8,14 @@ if (Get-Module -ListAvailable -Name AzureADPreview) {
 } 
 else {
     Write-Host "Module does not exist - Installing AzureADPreview Module"
-    Install-Module -Name AzureADPreview -Force
+    Install-Module -Name AzureADPreview -AllowClobber -Force
 }
+Import-Module AzureADPreview -Force
 
-#Connect to AzureAD and pull legacy Auth Sign-ins
+#Connect to AzureAD and pull successful legacy Auth Sign-ins
 Write-Host -f Yellow "Connecting to AzureAD...."
 Connect-AzureAD
-Get-AzureADAuditSignInLogs | where-object{$BasicAuths -eq $_.appDisplayName} | Format-Table -Property CreatedDateTime, UserPrincipalName, AppDisplayName, IPAddress, ClientAppUsed
+Get-AzureADAuditSignInLogs -Filter "status/errorCode eq 0" | where-object{$BasicAuths -eq $_.ClientAppUsed} | Format-Table -Property CreatedDateTime, UserPrincipalName, AppDisplayName, IPAddress, ClientAppUsed
 Write-Host -F Yellow "Script Complete - Press enter to disconnect from AzureAD"
 Pause
 Disconnect-AzureAD
