2c7e21291e3f734e38592d3dabe54ed7542b8637
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-01-01 18:48:50 -0600

    Add files via upload

---
+++ intune/BitLocker to Go Scripts/BL2GoToAAD.ps1
@@ -0,0 +1,6 @@
+﻿$USBDrives = Get-WmiObject -Class Win32_logicaldisk | where {$_.DriveType -eq "2"} | Select-Object DeviceID
+foreach ($DeviceID in $USBDrives)
+{
+$BLV = Get-BitLockerVolume -MountPoint $DeviceID.DeviceID
+BackupToAAD-BitLockerKeyProtector -MountPoint $DeviceID.DeviceID -KeyProtectorId $BLV.KeyProtector[1].KeyProtectorId -ErrorAction Continue
+}
