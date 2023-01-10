c15257aae0fa56ad7f4c9c70a5b6005c623e6d03
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-01-10 06:00:03 -0600

    Update Toast-CheckUserDownloadFiles.ps1

--- ToastNotifications/Toast-CheckUserDownloadFiles.ps1
+++ ToastNotifications/Toast-CheckUserDownloadFiles.ps1
@@ -1,6 +1,6 @@
 $DownloadsFiles = get-childitem -Path $env:USERPROFILE\Downloads -File -Recurse | Where-Object {$_.Length -gt 1MB} | Where-Object {$_.CreationTime -lt (Get-Date).AddDays(-1)}
 $AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
-$XMLString = Get-Content -Path C:\Users\gpnov\OneDrive\Desktop\CustomNotifications\NotificationXML.xml
+$XMLString = Get-Content -Path C:\temp\NotificationXML.xml
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
 [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]
 $ToastXml = [Windows.Data.Xml.Dom.XmlDocument]::new()
