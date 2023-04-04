820568bfeec49d8e9a20981b332d4b552ae0afd6
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-04-04 06:00:55 -0500

    Add files via upload

---
+++ autopilot/Post-ESP-Task/Windows11/Install.ps1
@@ -0,0 +1,4 @@
+$tempdir = "c:\temp"
+New-Item $tempdir -ItemType Directory -Force
+Copy-Item ".\PostESP-Script.ps1" -Destination $tempdir -Force
+Register-ScheduledTask -xml (Get-Content '.\PostESP-Script.xml' | Out-String) -TaskName "PostESP-Script" -Force
