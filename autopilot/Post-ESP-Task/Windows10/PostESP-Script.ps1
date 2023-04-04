d79885d77bb8258eafa9c168dd7a00e569337f0a
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-04-04 05:55:55 -0500

    Add files via upload

---
+++ autopilot/Post-ESP-Task/Windows10/PostESP-Script.ps1
@@ -0,0 +1,11 @@
+﻿$tempdir = "C:\temp"
+$taskdir = "C:\windows\system32\Tasks"
+New-Item $tempdir -ItemType Directory -Force
+New-Item $taskdir -ItemType File -Name ESP-TaskComplete -Force
+Start-Transcript -Path "C:\temp\post-esp-task.txt"
+start-sleep -Seconds 15
+write-output "Waiting for the cloud experience host broker process to complete..."
+wait-process "wwahost" -Verbose
+Write-Output "Process ended. Restarting Workstation"
+disable-scheduledtask -taskname PostESP-Script
+Restart-Computer -Force
