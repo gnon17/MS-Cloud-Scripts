8ad170669260283bdcf9689dd31d5f78fdd5163d
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-04-08 08:04:16 -0500

    Delete autopilot/Post-ESP-Task/Windows11 directory

--- autopilot/Post-ESP-Task/Windows11/Install.ps1
+++
@@ -1,4 +0,0 @@
-$tempdir = "c:\temp"
-New-Item $tempdir -ItemType Directory -Force
-Copy-Item ".\PostESP-Script.ps1" -Destination $tempdir -Force
-Register-ScheduledTask -xml (Get-Content '.\PostESP-Script.xml' | Out-String) -TaskName "PostESP-Script" -Force
