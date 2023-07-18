345f18ffda53ae6064e3e62c338e11b4b953a26e
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-07-18 06:46:53 -0500

    Add files via upload

---
+++ intune/Teams-Background-PR/Detection.ps1
@@ -0,0 +1,31 @@
+﻿#Checks for Az.Storage module and installs if it doesn't exist
+Write-Host "Checking for Az.Storage Module..." -ForegroundColor Green 
+$azstoragemodule = Get-Module -name Az.Storage
+if ($azstoragemodule -eq $null) {
+Install-PackageProvider -Scope CurrentUser -Name NuGet -MinimumVersion 2.8.5.201 -Force
+install-module az.storage -scope currentuser -Force
+}
+
+$BlobURL = "https://smbtothecloudblob.blob.core.windows.net/"
+$container = 'backgrounds'
+$storageaccount = New-AzStorageContext -Anonymous -BlobEndpoint $BlobURL
+$blobs = Get-AzStorageBlob -Container $container -Context $storageaccount
+$Images = $Blobs.name
+$BackgroundDir = "$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads"
+$Backgrounds = Get-ChildItem $BackgroundDir | Select -ExpandProperty Name -ErrorAction SilentlyContinue
+
+$FileComparison = 
+foreach ($image in $images)
+{
+$image -in $backgrounds
+}
+
+Write-Host "Checking to see if blob container images already exist..." -ForegroundColor Green 
+If ($FileComparison -contains $False) {
+Write-Output "Background Images are Missing"
+exit 1
+}
+else {
+Write-Output "All background images exist"
+exit 0
+}
---
+++ intune/Teams-Background-PR/Remediation.ps1
@@ -0,0 +1,24 @@
+﻿#Check for Teams Backgrounds Directory and create if it doesn't exist
+Write-Host "Checking for Teams background directory..." -ForegroundColor Green
+$BackgroundDir = "$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads"
+$DirExists = Test-Path $BackgroundDir
+If ($DirExists -ne $True) {
+New-Item -Path "$env:APPDATA\Microsoft\Teams\Backgrounds" -Name Uploads -ItemType Directory
+}
+
+#Checks for Az.Storage module and installs if it doesn't exist
+Write-Host "Checking for Az.Storage Module..." -ForegroundColor Green
+$azstoragemodule = Get-Module -name Az.Storage
+if ($azstoragemodule -eq $null) {
+install-module az.storage -scope currentuser -Force
+}
+
+#Variables for blob connection and download
+$BlobURL = "https://smbtothecloudblob.blob.core.windows.net/"
+$container = 'backgrounds'
+$storageaccount = New-AzStorageContext -Anonymous -BlobEndpoint $BlobURL
+$blobs = Get-AzStorageBlob -Container $container -Context $storageaccount
+foreach ($blob in $blobs)
+{
+Get-AzStorageBlobContent -Container $container -Blob $blob.Name -Destination $BackgroundDir -Context $storageaccount -Force
+}
