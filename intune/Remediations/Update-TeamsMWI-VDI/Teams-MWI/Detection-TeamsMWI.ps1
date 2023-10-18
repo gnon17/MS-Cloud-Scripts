844ed296c16edf689581733b686ac9912a81d1fa
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-10-18 06:02:13 -0500

    Add files via upload

---
+++ intune/Remediations/Update-TeamsMWI-VDI/Teams-MWI/Detection-TeamsMWI.ps1
@@ -0,0 +1,30 @@
+﻿$path = "c:\temp"
+$TempDir = Test-Path $path
+$teamsmsidest = "c:\temp\teamsinstaller.msi"
+$TeamsDownload = "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true"
+
+#Check for Temp Directory and create if it does not exist
+If ($TempDir -eq $False) {
+    New-Item -ItemType Directory -Path C:\Temp -Force
+}
+else {
+    write-host "Temp Directory Already Exists"
+}
+start-BitsTransfer -Source $TeamsDownload -Destination $teamsmsidest
+
+#Get Version of installed TeamsMWI and newest MSI
+$TeamsMWIVersion = get-package -name 'Teams Machine-Wide Installer' | select -ExpandProperty Version
+$TeamsMSI = get-applockerfileinformation -path $teamsmsidest | Select -expandproperty Publisher
+$TeamsMSIVersion = $TeamsMSI.BinaryVersion
+
+#Compare Teams Version
+If ($TeamsMWIVersion -ge $TeamsMSIVersion) {
+Write-Output "Teams is running the latest version"
+exit 0
+}
+Else {
+Write-Output "Teams needs to be updated"
+exit 1
+}
+
+Remove-Item -Path $teamsmsidest
