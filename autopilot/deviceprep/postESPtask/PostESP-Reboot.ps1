6e3810ea05998a5c38d07afff2d96e6c4e52bd7e
gnon17 <105109259+gnon17@users.noreply.github.com>
2024-06-12 08:15:19 -0500

    Update PostESP-Reboot.ps1

--- autopilot/deviceprep/postESPtask/PostESP-Reboot.ps1
+++ autopilot/deviceprep/postESPtask/PostESP-Reboot.ps1
@@ -3,7 +3,6 @@ $taskdir = "C:\windows\system32\Tasks"
 New-Item $tempdir -ItemType Directory -Force
 New-Item $taskdir -ItemType File -Name ESP-TaskComplete -Force
 Start-Transcript -Path "C:\temp\post-esp-task.txt" -Verbose
-disable-scheduledtask -taskname PostESP-Script -ErrorAction SilentlyContinue -Verbose
+disable-scheduledtask -taskname PostESP-Reboot -ErrorAction SilentlyContinue -Verbose
 #Unregister-ScheduledTask -TaskName PostESP-Script
-start-sleep -Seconds 1
 Restart-Computer -Force -Verbose
