87dcb1b1dea3817df78843e666e9eaf25ab9bb36
gnon17 <105109259+gnon17@users.noreply.github.com>
2024-06-12 06:37:52 -0500

    Update PostESP-Reboot.ps1

--- autopilot/deviceprep/postESPtask/PostESP-Reboot.ps1
+++ autopilot/deviceprep/postESPtask/PostESP-Reboot.ps1
@@ -1,10 +1,9 @@
 $tempdir = "C:\temp"
 $taskdir = "C:\windows\system32\Tasks"
-#$proc = "appidpolicyconverter"
 New-Item $tempdir -ItemType Directory -Force
 New-Item $taskdir -ItemType File -Name ESP-TaskComplete -Force
 Start-Transcript -Path "C:\temp\post-esp-task.txt" -Verbose
 disable-scheduledtask -taskname PostESP-Script -ErrorAction SilentlyContinue -Verbose
 #Unregister-ScheduledTask -TaskName PostESP-Script
-start-sleep -Seconds 2
+start-sleep -Seconds 1
 Restart-Computer -Force -Verbose
