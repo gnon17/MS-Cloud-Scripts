0c4682aeb09588a097d7a6a956a36537c54a210a
gnon17 <105109259+gnon17@users.noreply.github.com>
2024-06-13 06:41:34 -0500

    Delete autopilot/deviceprep/postESPtask directory

--- autopilot/deviceprep/postESPtask/PostESP-Reboot.ps1
+++
@@ -1,8 +0,0 @@
-$tempdir = "C:\temp"
-$taskdir = "C:\windows\system32\Tasks"
-New-Item $tempdir -ItemType Directory -Force
-New-Item $taskdir -ItemType File -Name ESP-TaskComplete -Force
-Start-Transcript -Path "C:\temp\post-esp-task.txt" -Verbose
-disable-scheduledtask -taskname PostESP-Reboot -ErrorAction SilentlyContinue -Verbose
-#Unregister-ScheduledTask -TaskName PostESP-Reboot -Confirm:$false
-Restart-Computer -Force -Verbose
