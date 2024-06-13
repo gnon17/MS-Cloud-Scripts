ac8a4751ac0b5ca914999dec67af3ce6978f26cc
gnon17 <gannonpnovak@gmail.com>
2024-06-13 08:55:40 -0500

    fixing errors in file names

--- autopilot/APv2-PostESPReboot/Event4725/IntunePlatformScript-RebootPostESP.ps1
+++ autopilot/APv2-PostESPReboot/Event4725/IntunePlatformScript-RebootPostESP.ps1
@@ -6,8 +6,8 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Script
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Toast.ps1" -OutFile .\toast.ps1
 
 #Grab the ScheduledTask XMLs
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Post-ESP-Reboot.xml" -OutFile .\Post-ESP-Reboot.xml.xml
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Post-ESP-Reboot-Notification.xml" -OutFile .\PostESP-Reboot-Notification.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Post-ESP-Reboot.xml" -OutFile .\Post-ESP-Reboot.xml
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Post-ESP-Reboot-Notification.xml" -OutFile .\Post-ESP-Reboot-Notification.xml
 
 #Move the action scripts and register the tasks
 Copy-Item ".\Post-ESP-Reboot.ps1" -Destination $tempdir -Force
