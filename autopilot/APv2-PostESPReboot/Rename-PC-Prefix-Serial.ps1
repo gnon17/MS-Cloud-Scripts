a42742c69fa26a0333c42011767bc16fa48c9fa3
gnon17 <gannonpnovak@gmail.com>
2024-06-13 14:17:55 -0500

    adding

---
+++ autopilot/APv2-PostESPReboot/Rename-PC-Prefix-Serial.ps1
@@ -0,0 +1,19 @@
+﻿$NamePrefix = "SMB-"
+$fullserial = Get-WmiObject win32_bios | select-object -expandproperty "Serialnumber"
+If ($fullserial.length -gt 10) {
+$shortserial = $fullserial.SubString(0,10)
+}
+$full = ($NamePrefix) + ($FullSerial)
+$short = ($NamePrefix)+($ShortSerial)
+
+Try {
+If ($fullserial.Length -le 11) {
+Rename-Computer -NewName $full
+}
+else {
+rename-computer -NewName $short
+}
+}
+Catch {
+Write-Host $_
+}
