cfac315fccc2f50d4ce10b99db0039a7d360723b
gnon17 <gannonpnovak@gmail.com>
2024-06-13 08:38:14 -0500

    fixing file name in URL

--- autopilot/APv2-PostESPReboot/Event4725/IntunePlatformScript-RebootPostESP.ps1
+++ autopilot/APv2-PostESPReboot/Event4725/IntunePlatformScript-RebootPostESP.ps1
@@ -3,7 +3,7 @@ New-Item $tempdir -ItemType Directory -Force
 
 #Grab the action scripts
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Post-ESP-Reboot.ps1" -OutFile .\Post-ESP-Reboot.ps1
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/toast.ps1" -OutFile .\toast.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Toast.ps1" -OutFile .\toast.ps1
 
 #Grab the ScheduledTask XMLs
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/gnon17/MS-Cloud-Scripts/main/autopilot/APv2-PostESPReboot/Event4725/Post-ESP-Reboot.xml" -OutFile .\Post-ESP-Reboot.xml.xml
