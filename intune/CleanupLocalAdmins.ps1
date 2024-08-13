6cca4197392bb5b50214875ceec1baa0124f349d
Gannon Novak <gpnovak@outlook.com>
2024-08-13 08:20:47 -0500

    uploading script

---
+++ intune/CleanupLocalAdmins.ps1
@@ -0,0 +1,10 @@
+﻿$localadmins = Get-LocalGroupMember -Group "Administrators" | Where Name -ne "$env:computername\administrator" | Select-Object -ExpandProperty Name
+ForEach ($localadmin in $localadmins) {
+Try {
+Remove-LocalGroupMember -Group "Administrators" -Member $localadmin -ErrorAction Continue
+}
+Catch {
+	Write-Host -ForegroundColor Red "While removing local admin account"
+	Write-Host -ForegroundColor Red $_
+}
+}
