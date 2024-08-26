d8a0e3d9247bad44f6505a778c50f2d01e405ca2
Gannon Novak <gpnovak@outlook.com>
2024-08-26 16:31:14 -0500

    adding

---
+++ intune/Chrome-CloudAPAuthEnabled.ps1
@@ -0,0 +1,2 @@
+﻿New-Item -Path "HKLM:\Software\Policies\Google\Chrome" -Force
+New-ItemProperty -Path "HKLM:\Software\Policies\Google\Chrome" -Name CloudAPAuthEnabled -PropertyType DWORD -Value 1 -Force
