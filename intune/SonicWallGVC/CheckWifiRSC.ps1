5a525a678760f852b565873d099ca97220df65f3
Gannon Novak <gpnovak@outlook.com>
2024-09-23 14:14:36 -0500

    fixing names again

--- intune/SonicWallGVC/CheckWifiRSC.ps1
+++
@@ -1,9 +0,0 @@
-$wifinetadapters = Get-NetAdapterRsc | Where-Object Name -Match "Wi-Fi" | Select-Object -ExpandProperty "IPv4Enabled" | Out-String
-If ($wifinetadapters -match "True") {
-    Write-Output "RSC is enabled on at least one WiFi Adapter"
-    Exit 1
-}
-Else {
-    Write-Output "RSC is disabled on all wifi adapters"
-    exit 0
-}
