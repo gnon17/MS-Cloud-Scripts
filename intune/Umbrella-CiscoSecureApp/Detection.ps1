766068213ec7dde7d2f2ad871c4cbe219ce9ec6b
gnon17 <105109259+gnon17@users.noreply.github.com>
2024-10-16 14:02:45 -0500

    added DART detection

--- intune/Umbrella-CiscoSecureApp/Detection.ps1
+++ intune/Umbrella-CiscoSecureApp/Detection.ps1
@@ -1,7 +1,8 @@
+$dart = get-package -name 'Cisco Secure Client - Diagnostics and Reporting Tool' -ErrorAction SilentlyContinue
 $JsonFile = Test-Path "$env:ProgramData\Cisco\Cisco Secure Client\Umbrella\OrgInfo.json"
 $UmbrellaService = Get-Service -Name 'csc_umbrellaagent' -ErrorAction SilentlyContinue
 $VPNService = Get-Service -Name 'csc_vpnagent' -ErrorAction SilentlyContinue
-If ($UmbrellaService -and $VPNService -and $JsonFile) {
+If ($UmbrellaService -and $VPNService -and $JsonFile -and $dart) {
 Write-Output "Detected"
 Exit 0
 }
