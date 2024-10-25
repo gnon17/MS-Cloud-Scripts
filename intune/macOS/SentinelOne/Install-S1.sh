b558ec82a9879bcc76cdc7b0db31001a7a8b37b6
Gannon Novak <gpnovak@outlook.com>
2024-10-25 16:12:48 -0500

    fixed variables

--- intune/macOS/SentinelOne/Install-S1.sh
+++ intune/macOS/SentinelOne/Install-S1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-siteToken="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-installerurl="https://yourstorageaccount.blob.core.windows.net/test/Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
+siteToken="eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS1jdzAyLnNlbnRpbmVsb25lLm5ldCIsICJzaXRlX2tleSI6ICJhZGU1ZWVlNGNkN2JhMTcwIn0="
+installerurl="https://customdomaintest1717.blob.core.windows.net/test/Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
 installer="Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
 dir="/tmp/"
 tokenfile="com.sentinelone.registration-token"
@@ -12,13 +12,13 @@ then
 
 else
 #Download installer into tmp directory
-curl -L -o /tmp/$installer $installerurl
+curl -L -o $dir$installer $installerurl
 
 #Create Site Token File
 echo $siteToken > $dir$tokenfile
 
 #Install Agent
-/usr/sbin/installer -pkg /tmp/$pkgName -target /
+/usr/sbin/installer -pkg $dir$installer -target /
 
 #Cleanup token file
 rm -f $dir$tokenfile
