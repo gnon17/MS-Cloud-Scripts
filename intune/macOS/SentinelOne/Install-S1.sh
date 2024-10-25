c996756388bc73b88f4976f468fe615431280168
Gannon Novak <gpnovak@outlook.com>
2024-10-25 16:13:30 -0500

    sanitized

--- intune/macOS/SentinelOne/Install-S1.sh
+++ intune/macOS/SentinelOne/Install-S1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-siteToken="eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS1jdzAyLnNlbnRpbmVsb25lLm5ldCIsICJzaXRlX2tleSI6ICJhZGU1ZWVlNGNkN2JhMTcwIn0="
-installerurl="https://customdomaintest1717.blob.core.windows.net/test/Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
+siteToken="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+installerurl="https://yourstorageaccount.blob.core.windows.net/test/Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
 installer="Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
 dir="/tmp/"
 tokenfile="com.sentinelone.registration-token"
