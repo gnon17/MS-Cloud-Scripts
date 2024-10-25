b76e045a1b770a8023c772ce6b1d1557d43f402a
Gannon Novak <gpnovak@outlook.com>
2024-10-25 16:16:50 -0500

    uploading

---
+++ intune/macOS/S1/Install-S1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+siteToken="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+installerurl="https://yourstorageaccount.blob.core.windows.net/test/Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
+installer="Sentinel-Release-24-2-2-7632_macos_v24_2_2_7632.pkg"
+dir="/tmp/"
+tokenfile="com.sentinelone.registration-token"
+
+if [ -d /Applications/SentinelOne/ ];
+then
+  echo "SentinelOne is already Installed"
+  exit 0
+
+else
+#Download installer into tmp directory
+curl -L -o $dir$installer $installerurl
+
+#Create Site Token File
+echo $siteToken > $dir$tokenfile
+
+#Install Agent
+/usr/sbin/installer -pkg $dir$installer -target /
+
+#Cleanup token file
+rm -f $dir$tokenfile
+fi
