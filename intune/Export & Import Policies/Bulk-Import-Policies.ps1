f64edc519907f4f14ee20a6de536272c4735c425
Gannon Novak <gpnovak@outlook.com>
2025-02-19 06:37:21 -0600

    fixing custom compliance

--- intune/Export & Import Policies/Bulk-Import-Policies.ps1
+++ intune/Export & Import Policies/Bulk-Import-Policies.ps1
@@ -28,70 +28,253 @@ $policyfiles = Get-ChildItem $jsonpath -Filter "*.json" -Recurse
 
 Connect-MgGraph -scopes $scopes
 
+#Set Arrays for policy gathering
+$namedLocations = @()
+$configurationPolicies = @()
+$managedapppolicies = @()
+$deviceConfigurations = @()
+$deviceHealthScripts = @()
+$deviceManagementScripts = @()
+$assignmentFilters = @()
+$deviceComplianceScripts = @()
+$deviceCompliancePolicies = @()
+
+#Determine policytypes and group them together in respective array
 Foreach ($policyfile in $policyfiles) {
 $policydata = Get-Content -path $policyfile.FullName | ConvertFrom-JSON | Select-Object -ExpandProperty '@odata.context' -ErrorAction SilentlyContinue
-
-## Settings Catalog Policies ##
+If ($policydata -match "namedLocations") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $namedLocations += $policy
+}
 If ($policydata -match "configurationPolicies") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $configurationPolicies += $policy
+}
+If ($policydata -match "managedAppPolicies") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $managedapppolicies += $policy
+}
+If ($policydata -match "deviceConfigurations") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $deviceConfigurations += $policy
+}
+If ($policydata -match "deviceHealthScripts") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $deviceHealthScripts += $policy
+}
+If ($policydata -match "deviceManagementScripts") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $deviceManagementScripts += $policy
+}
+If ($policydata -match "assignmentFilters") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $assignmentFilters += $policy
+}
+If ($policydata -match "deviceComplianceScripts") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $deviceComplianceScripts += $policy
+}
+If ($policydata -match "deviceCompliancePolicies") {
+    $policy = Get-Content -path $policyfile | ConvertFrom-Json
+    $deviceCompliancePolicies += $policy
+}
+}
+
+## Named Locations ##
+Write-Host -ForegroundColor Cyan "Importing Named Locations"
+foreach ($namedLocation in $namedLocations) {
     Try {
-    Write-Host -ForegroundColor Yellow "Policy type is Settings Catalog Policy"
-    Write-Host -ForegroundColor Yellow "Trying to import "$policyfile.name""
-    $policy = Get-Content -path $policyfile
-    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/configurationPolicies -ContentType "application/json" -Body $policy
+    Write-Host -ForegroundColor Yellow "Trying to import"$namedLocation.displayName""
+    $policy = $namedLocation | ConvertTo-Json -Depth 20
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/conditionalAccess/namedLocations/ -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Successfully Imported"$namedLocation.displayName""
     }
     Catch {
-        Write-Host -ForegroundColor Red "There was an error importing $policyfile"
+        Write-Host -ForegroundColor Red "There was an error importing "$namedLocation.displayname""
+        Write-Host $_
+        }    
+}
+
+## Settings Catalog Policies ##
+Write-Host -ForegroundColor Cyan "Importing Settings Catalog Policies"
+foreach ($configurationpolicy in $configurationPolicies) {
+    Try {
+    Write-Host -ForegroundColor Yellow "Trying to import "$configurationpolicy.Name""
+    $policy = $configurationpolicy | ConvertTo-Json -Depth 20
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/ -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Successfully Imported"$configurationpolicy.Name""
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "There was an error importing "$configurationpolicy.Name""
         Write-Host $_
         }    
 }
      
 ## App Protection Policies ##
-If ($policydata -match "managedAppPolicies") {
+Write-Host -ForegroundColor Cyan "Importing App Protection Policies"
+foreach ($managedAppPolicy in $managedAppPolicies) {
     Try {
-    Write-Host -ForegroundColor Yellow "Policy type is App Protection Policy"
-    Write-Host -ForegroundColor Yellow "Trying to import "$policyfile.name""
-    $policy = Get-Content -path $policyfile
-    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceAppManagement/managedAppPolicies/ -ContentType "application/json" -Body $policy
+    Write-Host -ForegroundColor Yellow "Trying to import "$managedAppPolicy.displayname""
+    $policy = $managedAppPolicy | ConvertTo-Json -Depth 20
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceAppManagement/managedAppPolicies/ -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Successfully Imported"$managedAppPolicy.displayname""
     }
-    Catch {
-        Write-Host -ForegroundColor Red "There was an error importing $policyfile"
+        Catch {
+        Write-Host -ForegroundColor Red "There was an error importing"$managedAppPolicy.displayname""
         Write-Host $_
         }    
 }
 
 ## Custom Device Config Profiles ##
-If ($policydata -match "deviceConfigurations") {
+Write-Host -ForegroundColor Cyan "Importing Custom Device Configuration Profiles"
+foreach ($deviceConfiguration in $deviceConfigurations) {
     Try {
-    Write-Host -ForegroundColor Yellow "Policy type is Custom Device Configuration Policy"
-    Write-Host -ForegroundColor Yellow "Trying to import "$policyfile.name""
-    $policy = Get-Content -path $policyfile
+    Write-Host -ForegroundColor Yellow "Trying to import "$deviceConfiguration.displayName""
+    $policy = $deviceConfiguration | ConvertTo-JSON -Depth 20
     #getting errors when using Beta so using v1
-    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/v1.0/deviceManagement/deviceConfigurations -ContentType "application/json" -Body $policy
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/v1.0/deviceManagement/deviceConfigurations -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Successfully Imported"$deviceConfiguration.displayName""
     }
     Catch {
-        Write-Host -ForegroundColor Red "There was an error importing $policyfile"
+        Write-Host -ForegroundColor Red "There was an error importing"$deviceConfiguration.displayName""
         Write-Host $_
         }    
 }
 
 ## Remediations ##
-If ($policydata -match "deviceHealthScripts") {
+Write-Host -ForegroundColor Cyan "Importing Remediations"
+ForEach ($deviceHealthScript in $deviceHealthScripts) {
     Try {
-    Write-Host -ForegroundColor Yellow "Policy type is Remediation"
-    Write-Host -ForegroundColor Yellow "Trying to import "$policyfile.name""
-    $policy = Get-Content -path $policyfile
-    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/deviceHealthScripts/ -ContentType "application/json" -Body $policy
+    Write-Host -ForegroundColor Yellow "Trying to import"$deviceHealthScript.displayName""
+    $policy = $deviceHealthScript | ConvertTo-Json -Depth 20
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/deviceHealthScripts/ -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Successfully imported"$deviceHealthScript.displayName""
     }
     Catch {
-        Write-Host -ForegroundColor Red "There was an error importing $policyfile"
+        Write-Host -ForegroundColor Red "There was an error importing"$deviceHealthScript.displayName""
         Write-Host $_
         }    
 }
 
-else {
-    write-host "Policy type not supported for Import with this script"
+## Platform Scripts ##
+Write-Host -ForegroundColor Cyan "Importing Platform Scripts"
+ForEach ($deviceManagementScript in $deviceManagementScripts) {
+    Try {
+    Write-Host -ForegroundColor Yellow "Trying to import"$deviceManagementScript.displayname""
+    $policy = $deviceManagementScript | ConvertTo-Json -Depth 20
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/deviceManagementScripts/ -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Successfully Imported"$deviceManagementScript.displayname""
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "There was an error importing"$deviceManagementScript.displayname""
+        Write-Host $_
+        }    
+}
+
+## Tenant Filters ##
+Write-Host -ForegroundColor Cyan "Importing Tenant Filters"
+ForEach ($assignmentFilter in $assignmentFilters) {
+    Try {
+    Write-Host -ForegroundColor Yellow "Trying to import"$assignmentfilter.displayName""
+    $policy = $assignmentFilter | ConvertTo-Json -Depth 20
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/assignmentFilters/ -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Successfully imported"$assignmentfilter.displayName""
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "There was an error importing"$assignmentfilter.displayName""
+        Write-Host $_
+        }    
+}
+
+#Count current compliance policy scripts
+$URI = "https://graph.microsoft.com/beta/deviceManagement/deviceComplianceScripts/"
+$response = Invoke-MgGraphRequest -Method GET -Uri $uri
+$existingcount = $response.'@odata.count'
+
+If ($devicecompliancescripts.count -ne 0) {
+## Custom Compliance Scripts ##
+Write-Host -ForegroundColor Cyan "Importing Custom Compliance Scripts"
+ForEach ($deviceComplianceScript in $deviceComplianceScripts) {
+    Try {
+    Write-Host -ForegroundColor Yellow "Trying to import"$deviceComplianceScript.displayName""
+    $policy = $deviceComplianceScript | ConvertTo-Json -Depth 20
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/deviceComplianceScripts/ -ContentType "application/json" -Body $policy | Out-Null
+    Write-Host -ForegroundColor Green "Succesfully imported"$deviceComplianceScript.displayName""
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "There was an error importing"$deviceComplianceScript.displayName""
+        Write-Host $_
+        }    
+}
+
+$response = Invoke-MgGraphRequest -Method GET -Uri $uri
+$newcount = $response.'@odata.count'
+
+While ($newcount -ne $existingcount + $devicecompliancescripts.count) {
+    Write-Host -ForegroundColor Yellow "New compliance policy IDs not available yet. Checking again in five seconds..."
+    Start-Sleep -Seconds 5
+    $response = Invoke-MgGraphRequest -Method GET -Uri $uri
+    $newcount = $response.'@odata.count'
+}
+
+# Update Reference file with new script ID so imported compliance scripts are assigned correctly
+Write-Host -ForegroundColor Yellow "Updating reference file for custom compliance policy scripts"
+$allPolicies = @()
+$referencefile = Import-CSV -path C:\temp\compliance_policies\ComplianceScriptReference.csv
+$URI = "https://graph.microsoft.com/beta/deviceManagement/deviceComplianceScripts/"
+$response = Invoke-MgGraphRequest -Method GET -Uri $uri
+$policyIds = $response.value.id
+Foreach ($policyId in $PolicyIds) {
+$policy = Invoke-MgGraphRequest -Method GET -URI "$uri$policyId" -OutputType PSObject
+#$name = $policy.displayname
+$allpolicies += $policy 
 }
 }
+
+Write-Host -ForegroundColor Yellow "Matching compliance script to new ID"
+ForEach ($policy in $allPolicies) {
+    $compliancescriptname = $policy.displayName
+    $match = $Referencefile | Where-Object { $_.displayName -eq $compliancescriptname }
+    Try {    
+    If ($match) {
+            Write-Host -ForegroundColor Green "Found compliance scrpit match for $($policy.displayName)"
+            $match.NewCompliancePolicyID = $policy.ID
+        }
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "There was an error matching a compliance script for $($policy.displayName)"
+        Write-Host $_
+        }  
+}
+
+## Compliance Policies ##
+Write-Host -ForegroundColor Cyan "Importing Compliance Policies"
+ForEach ($deviceCompliancePolicy in $deviceCompliancePolicies) {
+    Try {
+    Write-Host -ForegroundColor Yellow "Trying to import"$deviceCompliancePolicy.DisplayName""
+    $policy = $deviceCompliancePolicy
+    if ($policy.deviceCompliancePolicyScript.deviceComplianceScriptId) {
+        $match = $referencefile | Where-Object { $_.ID -eq $policy.deviceCompliancePolicyScript.deviceComplianceScriptId }
+        Try {
+        If ($match) {
+            Write-Host -ForegroundColor Yellow "Editing compliance policy script ID for "$policy.displayname" "
+            $policy.deviceCompliancePolicyScript.deviceComplianceScriptId = $match.NewCompliancePolicyID
+        }
+        }   
+            Catch {
+            Write-Host -ForegroundColor Red "There was an error editing"$policy.displayname"to match the new compliance script ID"
+            Write-Host $_
+            }
+    }
+    $policyjson = $policy | ConvertTo-Json -Depth 15
+    Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/deviceCompliancePolicies/ -ContentType "application/json" -Body $policyjson | Out-Null
+    Write-Host -ForegroundColor Green "Successfully imported"$deviceCompliancePolicy.DisplayName""
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "There was an error importing$deviceCompliancePolicy.DisplayName"
+        Write-Host $_
+        }    
+    }
 
 Stop-Transcript
 Disconnect-MgGraph
--- intune/Export & Import Policies/Export-Intune-Policies.ps1
+++ intune/Export & Import Policies/Export-Intune-Policies.ps1
@@ -47,7 +47,10 @@ $uri = "https://graph.microsoft.com/beta/conditionalAccess/namedLocations/"
 $response = Invoke-MgGraphRequest -Method GET -Uri $uri
 $policyIds = $response.value.id
 Foreach ($policyId in $PolicyIds) {
-$policy = Invoke-MgGraphRequest -Method GET -URI $uri$policyId
+$policy = Invoke-MgGraphRequest -Method GET -URI $uri$policyId -OutputType PSObject
+$policy.PsObject.Properties.Remove("id")
+$policy.PsObject.Properties.Remove("ModifiedDateTime")
+$policy.PsObject.Properties.Remove("createdDateTime")
 $policyjson = $policy | ConvertTo-Json -Depth 15
 $name = $policy.displayname
 $policyJson | Out-File -FilePath "$path\$name.json" -Encoding utf8
@@ -162,6 +165,103 @@ $name = $policy.displayname
 $policyJson | Out-File -FilePath "$path\$name.json" -Encoding utf8
 write-host -ForegroundColor yellow "Exported $name successfully"
 }
+
+#Windows Platform Scripts
+$path = "C:\temp\PowerShell_Scripts"
+New-Item -Path $path -ItemType Directory -Force
+Write-Host -ForegroundColor Green "Exporting Windows PowerShell scripts to $path"
+$uri = "https://graph.microsoft.com/beta/deviceManagement/deviceManagementScripts/"
+$response = Invoke-MgGraphRequest -Method GET -Uri $uri
+$policyIds = $response.value.id
+Foreach ($policyId in $PolicyIds) {
+$policy = Invoke-MgGraphRequest -Method GET -URI $uri$policyId -OutputType PSObject
+$policy.PsObject.Properties.Remove("id")
+$policy.PsObject.Properties.Remove("lastModifiedDateTime")
+$policyjson = $policy | ConvertTo-Json -Depth 15
+$name = $policy.displayname
+$policyJson | Out-File -FilePath "$path\$name.json" -Encoding utf8
+write-host -ForegroundColor yellow "Exported $name successfully"
+}
+
+#Filters
+$path = "C:\temp\Filters"
+New-Item -Path $path -ItemType Directory -Force
+Write-Host -ForegroundColor Green "Exporting Filters to $path"
+$uri = "https://graph.microsoft.com/beta/deviceManagement/assignmentFilters/"
+$response = Invoke-MgGraphRequest -Method GET -Uri $uri
+$policyIds = $response.value.id
+Foreach ($policyId in $PolicyIds) {
+$policy = Invoke-MgGraphRequest -Method GET -URI $uri$policyId -OutputType PSObject
+$policy.PsObject.Properties.Remove("id")
+$policy.PsObject.Properties.Remove("lastModifiedDateTime")
+$policy.PsObject.Properties.Remove("createdDateTime")
+$policy.PsObject.Properties.Remove("payloads")
+$policyjson = $policy | ConvertTo-Json -Depth 15
+$name = $policy.displayname
+$policyJson | Out-File -FilePath "$path\$name.json" -Encoding utf8
+write-host -ForegroundColor yellow "Exported $name successfully"
+}
+
+### Custom Compliance Scripts
+$path = "C:\temp\custom_compliance_scripts"
+New-Item -Path $path -ItemType Directory -Force
+Write-Host -ForegroundColor Green "Exporting Compliance Policies to $path"
+$URI = "https://graph.microsoft.com/beta/deviceManagement/deviceComplianceScripts/"
+$response = Invoke-MgGraphRequest -Method GET -Uri $uri
+$policyIds = $response.value.id
+$ComplianceScriptReference = @()
+Foreach ($policyId in $PolicyIds) {
+$policy = Invoke-MgGraphRequest -Method GET -URI "$uri$policyId" -OutputType PSObject
+$name = $policy.displayname
+write-host -ForegroundColor yellow "Exported $name successfully"
+$ComplianceScriptReference += [PSCustomObject]@{
+    ID = $policyId
+    DisplayName = $name
+    CompliancePolicyName = $null
+    NewCompliancePolicyID = $null
+}
+$policy.PsObject.Properties.Remove("id")
+$policy.PsObject.Properties.Remove("lastModifiedDateTime")
+$policy.PsObject.Properties.Remove("createdDateTime")
+$policyjson = $policy | ConvertTo-Json -Depth 15
+$policyJson | Out-File -FilePath "$path\$name.json" -Encoding utf8
+}
+
+#Compliance Policies
+$compliancepolicypath = "C:\temp\compliance_policies"
+New-Item -Path $compliancepolicypath -ItemType Directory -Force
+Write-Host -ForegroundColor Green "Exporting Compliance Policies to $compliancepolicypath"
+$uri = "https://graph.microsoft.com/beta/deviceManagement/deviceCompliancePolicies/"
+$scheduledActionsConfig = "?`$expand=scheduledActionsForRule(`$expand=scheduledActionConfigurations)"
+$response = Invoke-MgGraphRequest -Method GET -Uri $uri
+$policyIds = $response.value.id
+$allPolicies = @()
+Foreach ($policyId in $PolicyIds) {
+$policy = Invoke-MgGraphRequest -Method GET -URI "$uri$policyId$scheduledActionsConfig" -OutputType PSObject
+$policy.PsObject.Properties.Remove("id")
+$policy.PsObject.Properties.Remove("lastModifiedDateTime")
+$policy.PsObject.Properties.Remove("createdDateTime")
+$policy.PsObject.Properties.Remove("scheduledActionsForRule@odata.context")
+($policy.scheduledActionsForRule[0]).PSObject.Properties.Remove('scheduledActionConfigurations@odata.context')
+$policy.scheduledActionsForRule[0].ruleName = "ComplianceRule"
+$policyjson = $policy | ConvertTo-Json -Depth 15
+$name = $policy.displayname
+$policyJson | Out-File -FilePath "$compliancepolicypath\$name.json" -Encoding utf8
+write-host -ForegroundColor yellow "Exported $name successfully"
+$allPolicies += $policy
+}
+
+ForEach ($policy in $allPolicies) {
+    $compliancescriptid = $policy.deviceCompliancePolicyScript.deviceComplianceScriptId
+    If ($compliancescriptid) {
+        $matchingscript = $ComplianceScriptReference | Where-Object { $_.ID -eq $compliancescriptid }
+        If ($matchingscript) {
+            Write-Host "Found compliance scrpit match for $($policy.displayName)"
+            $matchingscript.CompliancePolicyName = $policy.displayName
+        }
+    }
+}
+$ComplianceScriptReference | export-csv -Encoding utf8 -path $compliancepolicypath\ComplianceScriptReference.csv
 
 Stop-Transcript
 Disconnect-MgGraph
